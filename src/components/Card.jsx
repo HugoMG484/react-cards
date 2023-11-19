bd5d289552483c221db416e778760e0149c89425
HugoMG484 <145819493+HugoMG484@users.noreply.github.com>
2023-11-18 22:52:40 -0600

    Proyecto React

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Card.css";
+
+/*
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.svg" />
+      <h3>Python</h3>
+    </div>
+  );
+};
+*/
+
+const Card = ({ lang, img, fcolor, scolor }) => {
+  return (
+    //Agregando los estilos
+    <div
+      className="card"
+      style={{ background: `linear-gradient(to left, ${fcolor}, ${scolor})` }}
+    >
+      <img src={img} alt="lang.svg" />
+      <h3>{lang}</h3>
+    </div>
+  );
+};
+//Definiendo los proptypes de Card
+Card.propTypes = {
+  lang: PropTypes.string,
+  img: PropTypes.string,
+  fcolor: PropTypes.string,
+  scolor: PropTypes.string,
+};
+
+export default Card;
